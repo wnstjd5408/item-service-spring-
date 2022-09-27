8813a64cdd28fade41650b365bdcf2e893926e58
paka_coding <wnstjd5408@naver.com>
2022-09-27 17:43:51 +0900

    refactor : 상품 등록 수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,12 +5,10 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -41,9 +39,43 @@ public class BasicItemController {
     public String addForm(){
         return "basic/addForm";
     }
+//    @PostMapping("/add")
+//    public String save(@RequestParam String itemName,
+//                       @RequestParam int price,
+//                       @RequestParam Integer quantity,
+//                       Model model){
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+    public String save(@ModelAttribute("item") Item item, Model model){
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략가능
+
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model){
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략가능
+
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save(){
-        return "basic/addForm";
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
